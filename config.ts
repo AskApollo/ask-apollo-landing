8158e52586feb59b48777ae5bd27298aff7e6f0f
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2024-05-13 11:02:42 +0530

    updated config

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ const config = {
   appDescription:
     "AI powered Chatbots and Ticketing System to Boost efficiency, Save time and Reduce cost on customer support.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "cyanarrow.com",
+  domainName: "adyotatech.com",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
