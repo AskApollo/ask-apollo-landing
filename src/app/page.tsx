7c811d0d85c6a7a4e98e8b77e8ccbf23597a9384
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2024-07-27 21:06:18 +0530

    removed story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,3 @@
-// import Link from "next/link";
-// import ButtonSignin from "@/src/components/ButtonSignin";
-
-// export default function Page() {
-//   return (
-//     <>
-//       <header className="p-4 flex justify-end max-w-7xl mx-auto">
-//         <ButtonSignin text="Login" />
-//       </header>
-//       <main>
-//         <section className="flex flex-col items-center justify-center text-center gap-12 px-8 py-24">
-//           <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
-
-//           <p className="text-lg opacity-80">
-//             The start of your new startup... What are you gonna build?
-//           </p>
-
-//           <a
-//             className="btn btn-primary"
-//             href="https://shipfa.st/docs"
-//             target="_blank"
-//           >
-//             Documentation & tutorials{" "}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               viewBox="0 0 20 20"
-//               fill="currentColor"
-//               className="w-5 h-5"
-//             >
-//               <path
-//                 fillRule="evenodd"
-//                 d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-//                 clipRule="evenodd"
-//               />
-//             </svg>
-//           </a>
-
-//           <Link href="/blog" className="link link-hover text-sm">
-//             Fancy a blog?
-//           </Link>
-//         </section>
-//       </main>
-//     </>
-//   );
-// }
 
 import { Suspense } from 'react'
 import Header from "@/src/components/Header";
@@ -53,15 +8,10 @@ import Pricing from "@/src/components/Pricing";
 import FAQ from "@/src/components/FAQ";
 import CTA from "@/src/components/CTA";
 import Footer from "@/src/components/Footer";
-// import Testimonial from '@/src/components/Testimonials1';
-// import TestimonialRating from '@/src/components/TestimonialRating';
-// import Testimonials11 from '@/src/components/Testimonials11';
 import Testimonials3 from '@/src/components/Testimonials3';
-// import TestimonialsAvatars from '@/src/components/TestimonialsAvatars';
 import FeaturesListicle from '../components/FeaturesListicle';
 import WithWithout from '../components/WithWithout';
 import TechUsed from '../components/TechUsed';
-import FounderStory from '../components/FounderStory';
 import LiveDemo from '../components/Demo';
 import FeaturedOn from '../components/FeaturedOn';
 
@@ -81,14 +31,9 @@ export default function Home() {
         <FeaturesListicle />
         <LiveDemo />
         <Pricing />
-        {/* <Testimonial /> */}
-        {/* <TestimonialRating /> */}
-        {/* <Testimonials11 /> */}
         <Testimonials3 />
-        {/* <TestimonialsAvatars /> */}
         <FAQ />
         <CTA />
-        <FounderStory />
       </main>
       <Footer />
     </>
