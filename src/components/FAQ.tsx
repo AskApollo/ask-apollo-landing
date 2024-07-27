a0116d0193281c17d23ede9fb394edeb08e7dc50
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2024-07-27 16:57:54 +0530

    blob store updated

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,97 +1,9 @@
 "use client";
 
-import Link from "next/link";
-import { useRef, useState } from "react";
-import type { JSX } from "react";
-import config from "@/config";
 
 // <FAQ> component is a lsit of <Item> component
 // Just import the FAQ & add your FAQ content to the const faqList arrayy below.
 
-interface FAQItemProps {
-  question: string;
-  answer: JSX.Element;
-}
-
-const faqList: FAQItemProps[] = [
-  {
-    question: "What do I get exactly?",
-    answer: <p className="space-y-2 leading-relaxed">Loreum Ipseum</p>,
-  },
-  {
-    question: "Can I get a refund?",
-    answer: (
-      <p className="space-y-2 leading-relaxed">
-        Yes! You can request a refund within 7 days of your purchase. Reach out
-        by <Link href={`mailto:${config.mailgun.supportEmail}`} target="_blank" className=" underline">email</Link>.
-      </p>
-    ),
-  },
-  {
-    question: "I have another question",
-    answer: (
-      <p className="space-y-2 leading-relaxed">Cool, contact us by email</p>
-    ),
-  },
-];
-
-const FaqItem = ({ item }: { item: FAQItemProps }) => {
-  const accordion = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <li>
-      <button
-        className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-left border-t md:text-lg border-base-content/10"
-        onClick={(e) => {
-          e.preventDefault();
-          setIsOpen(!isOpen);
-        }}
-        aria-expanded={isOpen}
-      >
-        <span
-          className={`flex-1 text-base-content ${isOpen ? "text-primary" : ""}`}
-        >
-          {item?.question}
-        </span>
-        <svg
-          className={`flex-shrink-0 w-4 h-4 ml-auto fill-current`}
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${isOpen && "rotate-180"
-              }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${isOpen && "rotate-180 hidden"
-              }`}
-          />
-        </svg>
-      </button>
-
-      <div
-        ref={accordion}
-        className={`transition-all duration-300 ease-in-out opacity-80 overflow-hidden`}
-        style={
-          isOpen
-            ? { maxHeight: accordion?.current?.scrollHeight, opacity: 1 }
-            : { maxHeight: 0, opacity: 0 }
-        }
-      >
-        <div className="pb-5 leading-relaxed">{item?.answer}</div>
-      </div>
-    </li>
-  );
-};
 
 const FAQ = () => {
   return (
